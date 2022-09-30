bcdde2a3d0acd72dc75d409d9e1f1dabbf9c0758
devilking1606196 <devilking1606196@gmail.com>
2022-09-30 16:25:18 +0530

    space optimization

--- gfg/dp/frogJump.cpp
+++ gfg/dp/frogJump.cpp
@@ -15,10 +15,42 @@ int minEnergy(int height[],int n,vector<int> &dp)
     
     return dp[n]=min(l,r);
 }
+int frogJump(int height[],int n)
+{
+    vector<int>dp(n,0);
+    dp[0]=0; //base case
+    for(int i=1;i<n;i++)
+    {
+        int l=dp[i-1]+abs(height[i]-height[i-1]);
+        int r=INT_MAX;
+        if(i>1)
+        r=dp[i-2]+abs(height[i-2]-height[i]);
+        dp[i]=min(l,r);
+    }
+    return dp[n-1];
+}
+
+int frogJump1(int height[],int n) //space optimization
+{
+    int prev=0; //base case
+    int prev1=0;
+    for(int i=1;i<n;i++)
+    {
+        int l=prev+abs(height[i]-height[i-1]);
+        int r=INT_MAX;
+        if(i>1)
+        r=prev1+abs(height[i-2]-height[i]);
+        prev1=prev;
+        prev=min(l,r);
+    }
+    return prev;
+}
 int main()
 {
     int height[]={30,10,60,10,60,50};
     int n=sizeof(height)/sizeof(height[0]);
     vector<int>dp(n+1,-1);
-    cout<<minEnergy(height,n-1,dp);
+    cout<<minEnergy(height,n-1,dp)<<endl;
+    cout<<frogJump(height,n)<<endl;
+    cout<<frogJump1(height,n)<<endl;
 }
